131b7de1cab4076d4cc0182c838b343f8f1636b9
Eueniy <tkachev.eugeny@gmail.com>
2020-08-29 16:54:02 +0300

    Updates

---
+++ precache-manifest.eab11d0ef641e0bf02c5592d1188b81a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3823682dacd7c838933f32c69df0d7",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "8d0b5b03c12730fa8209",
+    "url": "/goit-react-hw-02-feedback/static/js/2.7e64ba7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.7e64ba7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b37a04b36d40bd99f05",
+    "url": "/goit-react-hw-02-feedback/static/js/main.9d7e11e4.chunk.js"
+  },
+  {
+    "revision": "08563db974adcf55fd77",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.4bffc42e.js"
+  }
+]);
